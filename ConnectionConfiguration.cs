5ea25abf9ac2b637c6acfe96e400630fb364de2d
Software Assassin <chris@softwareassassin.com>
2012-08-25 00:34:39 -0400

    Copyright
    Comments in place.

--- ConnectionConfiguration.cs
+++ ConnectionConfiguration.cs
@@ -1,4 +1,20 @@
-﻿namespace Assassin.Data
+﻿///
+/// Original Author: Software Assassin
+/// 
+/// GNU All-Permissive License:
+/// Copying and distribution of this file, with or without modification,
+/// are permitted in any medium without royalty provided the copyright
+/// notice and this notice are preserved.  This file is offered as-is,
+/// without any warranty.
+/// 
+/// Source code available at:
+/// https://github.com/SoftwareAssassin/AssassinLibrary
+/// 
+/// Professional support available at:
+/// http://www.softwareassassin.com
+/// 
+
+namespace Assassin.Data
 {
 	public class ConnectionConfiguration : Base
 	{
